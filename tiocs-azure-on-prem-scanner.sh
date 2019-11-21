e7da120c2f22a020e0ff8f5a93fa58e7cd8c25da
jamessmith <jamessmith>
2019-11-21 15:15:47 -0500

    Put build into shell script for now.

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 #This script expects the following environment variables to be set:
 # REPO, IMAGE, TAG, $TENABLEACCESSKEY, $TENABLESECRETKEY
 echo "Checking $IMAGE:$TAG and uploading results into cloud.tenable.com repo $REPO"
@@ -7,14 +8,14 @@ echo ""
 echo "Variables list:"
 set
 
-docker build ./ -t $(Build.BuildId)
+echo "Build image"
+docker build ./ -t $IMAGE:$BUILD_BUILDID
+
+echo "Download Tenable.io on-prem scanner"
 
 docker login --username pubread --password BXaXRD9n3uEWKkGgt56eHVD5h tenableio-docker-consec-local.jfrog.io
 docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 
 echo "Start of on-prem analysis"
-echo "cmdline: docker save $REPO/$IMAGE:$TAG | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TENABLEACCESSKEY -e TENABLE_SECRET_KEY=$TENABLESECRETKEY -e IMPORT_REPO_NAME=$REPO -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGE:$TAG"
-
-docker save $REPO/$IMAGE:$TAG | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TENABLEACCESSKEY -e TENABLE_SECRET_KEY=$TENABLESECRETKEY -e IMPORT_REPO_NAME=$REPO -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGE:$TAG
-
+docker save $IMAGE:$BUILD_BUILDID | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TENABLEACCESSKEY -e TENABLE_SECRET_KEY=$TENABLESECRETKEY -e IMPORT_REPO_NAME=$REPO -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGE:$TAG
 echo "End of on-prem analysis
