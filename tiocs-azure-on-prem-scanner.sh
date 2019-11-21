26731cfecee81e5c0d29492482db3fcf580a8578
jamessmith <jamessmith>
2019-11-21 15:10:58 -0500

    Debugging variables being sent by Azure

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -3,11 +3,18 @@
 # REPO, IMAGE, TAG, $TENABLEACCESSKEY, $TENABLESECRETKEY
 echo "Checking $IMAGE:$TAG and uploading results into cloud.tenable.com repo $REPO"
 echo "Tenable.io Access Key: $TENABLEACCESSKEY"
+echo ""
+echo "Variables list:"
+set
+
+docker build ./ -t $(Build.BuildId)
 
 docker login --username pubread --password BXaXRD9n3uEWKkGgt56eHVD5h tenableio-docker-consec-local.jfrog.io
 docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 
 echo "Start of on-prem analysis"
 echo "cmdline: docker save $REPO/$IMAGE:$TAG | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TENABLEACCESSKEY -e TENABLE_SECRET_KEY=$TENABLESECRETKEY -e IMPORT_REPO_NAME=$REPO -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGE:$TAG"
+
 docker save $REPO/$IMAGE:$TAG | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TENABLEACCESSKEY -e TENABLE_SECRET_KEY=$TENABLESECRETKEY -e IMPORT_REPO_NAME=$REPO -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGE:$TAG
+
 echo "End of on-prem analysis
