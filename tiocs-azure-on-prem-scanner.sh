ce7c0f8527f42312aab203d24e09a492d32fc293
jamessmith <jamessmith>
2019-11-21 14:08:42 -0500

    Fixing James's dumb typos

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #This script expects the following environment variables to be set:
 # REPO, IMAGE, TAG, $TENABLEACCESSKEY, $TENABLESECRETKEY
+echo "Checking $IMAGE:$TAG and uploading results into cloud.tenable.com repo $REPO"
+echo "Tenable.io Access Key: $TENABLEACCESSKEY"
+
+
 docker login --username pubread --password BXaXRD9n3uEWKkGgt56eHVD5h tenableio-docker-consec-local.jfrog.io
 docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 
 docker save $IMAGE:$TAG | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TENABLE_IO_ACCESS_KEY \
  -e TENABLE_SECRET_KEY=$TENABLE_IO_SECRET_KEY -e IMPORT_REPO_NAME=$REPO \
- -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
+ -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest \
  inspect-image $IMAGE:$TAG
 
-echo "Checking $IMAGE:$TAG and uploading results into cloud.tenable.com repo $REPO"
-echo "Tenable.io Access Key: $TENABLEACCESSKEY"
 docker save $IMAGE:$TAG | docker run -e DEBUG_MODE=true
 while [ 1 -eq 1 ]; do
   RESP=`curl -s --request GET --url "https://cloud.tenable.com/container-security/api/v1/compliancebyname?image=$IMAGE&repo=$REPO&tag=$TAG" \
