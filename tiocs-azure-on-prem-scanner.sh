8a7de4d7f10012a45f0c9c91c163349625c683d0
jamessmith <jamessmith>
2019-11-21 15:26:43 -0500

    Cleaning up script debugging information

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 #This script expects the following environment variables to be set:
 # REPO, IMAGE, TAG, $TENABLEACCESSKEY, $TENABLESECRETKEY
 echo "Checking $IMAGE:$TAG and uploading results into cloud.tenable.com repo $REPO"
@@ -9,7 +8,9 @@ echo "Variables list:"
 set
 
 echo "Build image"
+set -x
 docker build ./ -t $IMAGE:$BUILD_BUILDID
+set +x
 
 echo "Download Tenable.io on-prem scanner"
 
@@ -17,7 +18,9 @@ docker login --username pubread --password BXaXRD9n3uEWKkGgt56eHVD5h tenableio-d
 docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 
 echo "Start of on-prem analysis"
+set -x
 docker save $IMAGE:$BUILD_BUILDID | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TENABLEACCESSKEY -e TENABLE_SECRET_KEY=$TENABLESECRETKEY -e IMPORT_REPO_NAME=$REPO -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGE:$TAG
+set +x
 echo "End of on-prem analysis"
 
 echo "Download report on image"
@@ -25,6 +28,7 @@ while [ 1 -eq 1 ]; do
   RESP=`curl -s --request GET --url "https://cloud.tenable.com/container-security/api/v1/compliancebyname?image=$IMAGE&repo=$REPO&tag=$BUILD_BUILDID" \
   --header 'accept: application/json' --header "x-apikeys: accessKey=$TENABLE_IO_ACCESS_KEY;secretKey=$TENABLE_IO_SECRET_KEY" \
   | sed -n 's/.*\"status\":\"\([^\"]*\)\".*/\1/p'`
+  echo "Report status: $RESP"
   if [ "x$RESP" = "xpass" ] ; then
     echo "Container marked as PASSED by policy rules"
     exit 0
@@ -33,5 +37,6 @@ while [ 1 -eq 1 ]; do
     echo "Container marked as FAILED by policy rules"
     exit 1
   fi
+  echo "Waiting 30 seconds before checking again for report"
   sleep 30
 done
